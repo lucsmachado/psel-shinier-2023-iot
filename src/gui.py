1685f23c15116f30a31ee70c92c849006d8c7467
Lucas Machado <lcmachado.cs@gmail.com>
2023-08-01 17:35:24 -0300

    feat: added GUI

---
+++ src/gui.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter.ttk import *
+
+window = Tk()
+window.title("Processo Seletivo Shinier 2023")
+
+frm_name = Frame()
+
+lbl_name = Label(master=frm_name, text="Nome:")
+lbl_name.pack()
+
+lbl_candidate = Label(master=frm_name)
+
+ent_name = Entry(master=frm_name)
+ent_name.pack()
+
+def handle_submit():
+    name = ent_name.get()
+    ent_name.delete(0, END)
+    lbl_candidate["text"] = "Candidato processo seletivo Shinier Iot: " + name
+    lbl_candidate.pack()
+
+btn_submit = Button(
+    master=frm_name,
+    text="Click me",
+    command=handle_submit
+)
+btn_submit.pack()
+
+frm_name.pack()
+
+window.mainloop()
