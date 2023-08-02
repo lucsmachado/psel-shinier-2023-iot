31786b6c382171ece50f32dffce939fb7479b894
Lucas Machado <lcmachado.cs@gmail.com>
2023-08-02 15:48:38 -0300

    Update read_repo.py

--- src/read_repo.py
+++ src/read_repo.py
@@ -18,7 +18,7 @@ def read_repo():
         origin = repo.remotes.origin
         origin.pull()
         while True:
-            time.sleep(5)
+            time.sleep(30)
             origin.pull()
             print(f'Updated at {datetime.datetime.now()}')
     else:
