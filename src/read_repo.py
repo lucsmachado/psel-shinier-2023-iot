7f22a7308f3e314b6cf12b594dd454f76273e02a
Lucas Machado <lcmachado.cs@gmail.com>
2023-08-02 13:39:34 -0300

    feat: added scheduled updated task for every hour interval

--- src/read_repo.py
+++ src/read_repo.py
@@ -1,14 +1,28 @@
 import os
+import time
+import datetime
 from dotenv import load_dotenv
 from git import Repo
 
+def read_repo(path):
+    repo = Repo(path)
+    if not repo.bare:
+        print(f'Repo at {path} successfully loaded at {datetime.datetime.now()}.')
+        origin = repo.remotes.origin
+        origin.pull()
+        while True:
+            time.sleep(3600)
+            origin.pull()
+            print(f'Updated at {datetime.datetime.now()}')
+    else:
+        print(f'Could not load repo at {path}.')	
+
+
 if __name__ == "__main__":
-	load_dotenv()
-	repo_path = os.getenv('GIT_REPO_PATH')
-	repo = Repo(repo_path)
-	if not repo.bare:
-		print('Repo at {} successfully loaded.'.format(repo_path))
-		origin = repo.remotes.origin
-		origin.pull()
-	else:
-		print('Could not load repo at {}.'.format(repo_path))	
+    load_dotenv()
+    repo_path = os.getenv('GIT_REPO_PATH')
+
+    if repo_path == None:
+        raise FileNotFoundError('Environment variables .env file not found.\nPlease create one based on the provided .env.example and try again.')
+
+    read_repo(repo_path)
